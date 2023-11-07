a8fe580c442927b51c29bc10f9f757a63caa3aaf
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-07 09:22:46 +0900

    確認用ファイル

---
+++ js/test.js
@@ -0,0 +1,5 @@
+const test1 = document.getElementById("test1");
+const test2 = document.getElementById("test2");
+
+test1.innerHTML += "innerHTML : <p>初めまして</p>";
+test2.innerText += "innerText : <p>初めまして</p>";
